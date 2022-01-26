d1e02c4ccf56d581c793f97b2011f3b2b859551f
Ahmad Juhdi <ahmadjuhdi007@gmail.com>
2022-01-26 21:21:56 +0800

    Kategori

--- app/Config/Routes/Api.php
+++ app/Config/Routes/Api.php
@@ -3,6 +3,6 @@
 $route->post("user/register", 'Api\User::register');
 
 $route->resource("produk_beranda", ['controller' => 'Api\ProdukBeranda', 'only' => ['index', 'show']]);
-$route->resource("kategori", ['controller' => 'Api\KAtegori', 'only' => ['index', 'show']]);
+$route->resource("kategori", ['controller' => 'Api\Kategori', 'only' => ['index', 'show']]);
 $route->resource("produk", ['controller' => 'Api\Produk', 'only' => ['index', 'show']]);
 $route->resource("banner", ['controller' => 'Api\Banner', 'only' => ['index', 'show']]);
