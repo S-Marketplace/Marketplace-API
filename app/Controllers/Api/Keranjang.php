1c4af54ff14bab024fedb5ecce0c21a571e6d53b
Ahmad Juhdi <ahmadjuhdi007@gmail.com>
2022-01-26 23:27:07 +0800

    Fix Keranjang

--- app/Controllers/Api/Keranjang.php
+++ app/Controllers/Api/Keranjang.php
@@ -1,5 +1,7 @@
 <?php namespace App\Controllers\Api;
 
+use App\Models\KategoriModel;
+use App\Models\ProdukGambarModel;
 use App\Controllers\MyResourceController;
 /**
  * Class Keranjang
@@ -35,6 +37,35 @@ class Keranjang extends MyResourceController
         $this->model->select('*');
         $this->model->where(['krjUserEmail' => $this->user['email']]);
         $this->model->with(['products']);
-        return parent::index();
+        
+        $this->applyQueryFilter();
+        $limit = $this->request->getGet("limit") ? $this->request->getGet("limit") : $this->defaultLimitData;
+        $offset = $this->request->getGet("offset") ? $this->request->getGet("offset") : 0;
+        if ($limit != "-1") {
+            $this->model->limit($limit);
+        }
+        $this->model->offset($offset);
+        
+        $data = $this->model->find();
+
+        $data = array_map(function($e){
+            $e = $e;
+            $produkGambarModel = new ProdukGambarModel();
+            $kategoriModel = new KategoriModel();
+            $combine['kategori'] = $kategoriModel->find($e->products->kategoriId);
+            $combine['gambar'] = $produkGambarModel->where(['prdgbrProdukId' =>$e->products->id])->find();
+            $e->products = array_merge((array)$e->products, $combine);
+            return $e;
+        }, $data);
+      
+        try {
+            return $this->response([
+                'rows' => $data,
+                'limit' => $limit,
+                'offset' => $offset,
+            ]);
+        } catch (\Exception $ex) {
+            return $this->response(null, 500, $ex->getMessage());
+        }
     }
 }
