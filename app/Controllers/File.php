e42a286aed11b835a1f9697bae8b715dc48d7494
Ahmad Juhdi <ahmadjuhdi007@gmail.com>
2022-06-11 10:33:12 +0800

    Push

--- app/Controllers/File.php
+++ app/Controllers/File.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 
 
 use Bepsvpt\Blurhash\BlurHash;
+use kornrunner\Blurhash\Blurhash as KornBlurHash;
 use CodeIgniter\Config\Config;
 use App\Controllers\BaseController;
 use App\Libraries\MyDownloadResponse;
@@ -30,12 +31,12 @@ class File extends BaseController
                 $fileName = $realFileName . '_' . $size . '.' . $file->getExtension();
                 $newCompressPath = $path . $realFileName;
                 $newCompressPathFile = $newCompressPath . DIRECTORY_SEPARATOR . $fileName;
-                
+
                 // Create folder thumbnail
                 if (!is_dir($newCompressPath)) {
                     mkdir($newCompressPath, 0777, true);
                 }
-    
+
                 // Compress if file not exist
                 if (!file_exists($newCompressPathFile)) {
                     $image = \Config\Services::image()
@@ -43,7 +44,7 @@ class File extends BaseController
                         ->resize($size, $size, true, 'height')
                         ->save($newCompressPathFile);
                 }
-    
+
                 $fullpath = $newCompressPathFile;
             }
         }
@@ -66,11 +67,11 @@ class File extends BaseController
             } else {
                 $filename = self::NOT_EXIST_IMAGE;
                 if (file_exists($filename)) {
-                    $mime = mime_content_type($filename); 
-                    header('Content-Length: ' . filesize($filename)); 
-                    header("Content-Type: $mime");  
-                    header('Content-Disposition: inline; filename="' . $filename . '";'); 
-                    readfile($filename); 
+                    $mime = mime_content_type($filename);
+                    header('Content-Length: ' . filesize($filename));
+                    header("Content-Type: $mime");
+                    header('Content-Disposition: inline; filename="' . $filename . '";');
+                    readfile($filename);
                     exit();
                 }
 
@@ -79,7 +80,7 @@ class File extends BaseController
                     'message' => 'File not found'
                 ]);
             }
-        } else if ($type == 'blur') {
+        } else if ($type == 'blurhash') {
             // Blurhash image
             if (file_exists($fullpath)) {
                 $file = new \CodeIgniter\Files\File($fullpath);
@@ -90,6 +91,12 @@ class File extends BaseController
                 $image = imagecreatefromstring(file_get_contents(self::NOT_EXIST_IMAGE));
                 $blurHash = new BlurHash(4, 3);
                 echo $blurHash->encode($image);
+            }
+        } else if ($type == 'blur') {
+            if (file_exists($fullpath)) {
+                $this->blurImage($fullpath);
+            } else {
+                $this->blurImage((self::NOT_EXIST_IMAGE));
             }
         }
     }
@@ -102,17 +109,18 @@ class File extends BaseController
      * @param [type] $dir
      * @return void
      */
-    public function checkNotUsedFile($dir){
+    public function checkNotUsedFile($dir)
+    {
         $usedFile = [];
 
-        if(!empty($usedFile)){
+        if (!empty($usedFile)) {
             $path  = Config::get("App")->uploadPath . "$dir" . DIRECTORY_SEPARATOR;
             $files = scandir($path);
             $mergePath = array_flip($files);
-            unset($mergePath['.'],$mergePath['..']);
-    
+            unset($mergePath['.'], $mergePath['..']);
+
             foreach ($mergePath as $fileName => $value) {
-                if(!preg_grep('~' . $fileName . '~', $usedFile)){
+                if (!preg_grep('~' . $fileName . '~', $usedFile)) {
                     try {
                         unlink($path . $fileName);
                     } catch (\Throwable $th) {
@@ -123,26 +131,85 @@ class File extends BaseController
         }
     }
 
-    private function deleteDirectory($dir) {
+    /**
+     * Recursive Delete Directory
+     *
+     * @param [type] $dir
+     * @return void
+     */
+    private function deleteDirectory($dir)
+    {
         if (!file_exists($dir)) {
             return true;
         }
-    
+
         if (!is_dir($dir)) {
             return unlink($dir);
         }
-    
+
         foreach (scandir($dir) as $item) {
             if ($item == '.' || $item == '..') {
                 continue;
             }
-    
+
             if (!$this->deleteDirectory($dir . DIRECTORY_SEPARATOR . $item)) {
                 return false;
             }
-    
         }
-    
+
         return rmdir($dir);
+    }
+
+    /**
+     * Blur Image
+     *
+     * @param [type] $path
+     * @return void
+     */
+    private function blurImage($path)
+    {
+        header("Content-Type: image/png");
+        $file = new \CodeIgniter\Files\File($path);
+        $image = imagecreatefromstring(file_get_contents($file));
+
+        /* Get original image size */
+        list($w, $h) = getimagesize($file);
+
+        /* Create array with width and height of down sized images */
+        $size = array(
+            'sm' => array('w' => intval($w / 4), 'h' => intval($h / 4)),
+            'md' => array('w' => intval($w / 2), 'h' => intval($h / 2))
+        );
+
+        /* Scale by 25% and apply Gaussian blur */
+        $sm = imagecreatetruecolor($size['sm']['w'], $size['sm']['h']);
+        imagecopyresampled($sm, $image, 0, 0, 0, 0, $size['sm']['w'], $size['sm']['h'], $w, $h);
+
+        for ($x = 1; $x <= 40; $x++) {
+            imagefilter($sm, IMG_FILTER_GAUSSIAN_BLUR, 999);
+        }
+
+        imagefilter($sm, IMG_FILTER_SMOOTH, 99);
+        imagefilter($sm, IMG_FILTER_BRIGHTNESS, 10);
+
+        /* Scale result by 200% and blur again */
+        $md = imagecreatetruecolor($size['md']['w'], $size['md']['h']);
+        imagecopyresampled($md, $sm, 0, 0, 0, 0, $size['md']['w'], $size['md']['h'], $size['sm']['w'], $size['sm']['h']);
+        imagedestroy($sm);
+
+        for ($x = 1; $x <= 25; $x++) {
+            imagefilter($md, IMG_FILTER_GAUSSIAN_BLUR, 999);
+        }
+
+        imagefilter($md, IMG_FILTER_SMOOTH, 99);
+        imagefilter($md, IMG_FILTER_BRIGHTNESS, 10);
+
+        /* Scale result back to original size */
+        imagecopyresampled($image, $md, 0, 0, 0, 0, $w, $h, $size['md']['w'], $size['md']['h']);
+        imagedestroy($md);
+
+        imagepng($image);
+        imagedestroy($image);
+        exit;
     }
 }
